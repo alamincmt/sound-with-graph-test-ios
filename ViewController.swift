be5ea1dbd9012d38d1c6a46f342d2bb653e1a390
Al Amin <alamin@Als-MacBook-Pro.local>
2023-04-13 23:27:16 +0600

    Project initalized.

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chart Test
+//
+//  Created by Al Amin on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
